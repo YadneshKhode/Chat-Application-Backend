baeb38efef7876e4a25060a5ebef1653c3322839
Yadnesh Khode <yadneshkhode@gmail.com>
2021-03-15 22:52:26 +0530

    ecosystem file updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,10 +9,10 @@ module.exports = {
 
   deploy : {
     production : {
-      user : 'SSH_USERNAME',
+      user : 'YadneshKhode',
       host : 'SSH_HOSTMACHINE',
       ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
+      repo : 'https://github.com/YadneshKhode/Chat-Application-Backend.git',
       path : 'DESTINATION_PATH',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
