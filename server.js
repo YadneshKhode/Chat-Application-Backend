1c72475c637878a16e1b1d476fe9c96fa0781a13
Yadnesh Khode <yadneshkhode@gmail.com>
2021-03-25 01:09:44 +0530

    trying to fix bugs on mobile devices

--- server.js
+++ server.js
@@ -1,50 +1,46 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 5000;
-const path = require("path");
-const publicDirectoryPath = path.join(
-  __dirname,
-  "../chat-application/client/public"
-);
 const http = require("http");
+const express = require("express");
 const socketio = require("socket.io");
 const cors = require("cors");
-const router = require("./router");
-const server = http.createServer(app);
-const io = socketio(server, {
-  cors: {
-    origin: "*",
-  },
-});
-app.use(router);
 const {
   addUser,
   removeUser,
   getUser,
   getUsersInRoom,
 } = require("./utils/users");
-const { generateMessage } = require("./utils/messages");
-app.use(express.json());
-app.use(express.static(publicDirectoryPath));
-app.use(cors());
+const router = require("./router");
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+app.use(router);
+const port = process.env.PORT || 5000;
+const path = require("path");
+const publicDirectoryPath = path.join(
+  __dirname,
+  "../chat-application/client/public"
+  );
+  const { generateMessage } = require("./utils/messages");
+  app.use(express.json());
+  app.use(express.static(publicDirectoryPath));
+  app.use(cors());
+  
+  //automatically triggered after scoket.join is called
 
-//automatically triggered after scoket.join is called
-
-io.on("connection", (socket) => {
-  socket.on("join", ({ username, room, displayPhoto, email }, callback) => {
-    const { error, user } = addUser({
-      id: socket.id,
-      username,
-      room,
-      displayPhoto,
-      email,
-    });
+  io.on("connection", (socket) => {
+    socket.on("join", ({ username, room, displayPhoto, email }, callback) => {
+      const { error, user } = addUser({
+        id: socket.id,
+        username,
+        room,
+        displayPhoto,
+        email,
+      });
     if (error) {
       return callback(error);
     }
-
+    
     socket.join(user.room); //emits only to those who are in this room in other words adds this specific connection ( socket ) into the room.
-
+    
     // triggered when user joins new room
     socket.emit(
       "message",
@@ -52,48 +48,48 @@ io.on("connection", (socket) => {
         "Admin",
         `Hello ${user.username}, welcome to the room ${user.room}`,
         room
-      )
-    );
-    io.to(user.room).emit("roomData", {
-      users: getUsersInRoom(user.room),
-    });
-    // triggered when user joins room
-    if (user) {
+        )
+        );
+        io.to(user.room).emit("roomData", {
+          users: getUsersInRoom(user.room),
+        });
+        // triggered when user joins room
+        if (user) {
       socket.broadcast
-        .to(user.room)
-        .emit(
+      .to(user.room)
+      .emit(
           "message",
           generateMessage("Admin", `${user.username} has joined!`, room)
-        );
-    }
-    io.to(user.room).emit("roomData", {
-      users: getUsersInRoom(user.room),
-    });
-
+          );
+        }
+        io.to(user.room).emit("roomData", {
+          users: getUsersInRoom(user.room),
+        });
+        
     callback();
   });
-
+  
   //Triggered after sending message
-
+  
   socket.on("sendMessage", (message, callback) => {
     const user = getUser(socket.id);
     if (user) {
       io.to(user.room).emit(
         "message",
         generateMessage(user.username, message, user.room)
-      );
+        );
     }
     callback();
   });
-
+  
   socket.on("disconnect", () => {
     const user = removeUser(socket.id);
     if (user) {
       io.to(user.room).emit(
         "message",
         generateMessage("Admin", `${user.username} has left !`, user.room)
-      );
-      io.to(user.room).emit("roomData", {
+        );
+        io.to(user.room).emit("roomData", {
         users: getUsersInRoom(user.room),
       });
     }
@@ -106,26 +102,26 @@ io.on("connection", (socket) => {
       io.to(user.room).emit(
         "message",
         generateMessage("Admin", `${user.username} has left !`, user.room)
-      );
+        );
       io.to(user.room).emit("roomData", {
         users: getUsersInRoom(user.room),
       });
     }
   });
-
+  
   //used to remove user when user leaves room
 
   socket.on("leftRoom", (room, username) => {
     const user = getUser(socket.id);
     console.log(user);
-
+    
     io.to(room).emit(
       "message",
       generateMessage("Admin", username + "has  left !", room)
-    );
+      );
+    });
   });
-});
-
-server.listen(port, () => {
-  console.log(`The server is up and running on port ${port}`);
-});
+  
+  server.listen(port, () => {
+    console.log(`The server is up and running on port ${port}`);
+  });
